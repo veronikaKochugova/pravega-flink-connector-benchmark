6af42a290df2ff2eb1811e5c0911b3d5349f4c20
kochuv <veronika.kochugova@gmail.ru>
2020-02-19 12:43:53 +0300

    convert scala -> java

--- src/main/java/event/EventPayloadGeneratorSource.java
+++ src/main/java/event/EventPayloadGeneratorSource.java
@@ -2,6 +2,9 @@ package event;
 
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.LockSupport;
+
 public class EventPayloadGeneratorSource extends RichParallelSourceFunction {
 
     boolean cancelled = false;
@@ -16,13 +19,13 @@ public class EventPayloadGeneratorSource extends RichParallelSourceFunction {
     @Override
     public void run(SourceContext ctx) throws Exception {
         long nextTimeNanos = 0L;
-        long delayNanos = (rate > 0) ? Long((1e9 / rate).toLong : 0L;
+        long delayNanos = (rate > 0) ? new Double(1e9 / rate).longValue() : 0L;
         while (!cancelled) {
             if (nextTimeNanos < System.nanoTime()) {
-                LockSupport parkNanos 1L
+                LockSupport.parkNanos(1L);
             } else {
-                nextTimeNanos = System.nanoTime() + delayNanos
-                ctx.collect(new Array[Byte] (size))
+                nextTimeNanos = System.nanoTime() + delayNanos;
+                ctx.collect(new ArrayList<Byte>(size));
             }
         }
     }
@@ -32,17 +35,3 @@ public class EventPayloadGeneratorSource extends RichParallelSourceFunction {
         cancelled = true;
     }
 }
-
-class EventPayloadGeneratorSource(size:Int,rate:Double)
-        extends RichParallelSourceFunction[Array[Byte]]{
-
-        var cancelled=false
-
-        override def run(ctx:SourceFunction.SourceContext[Array[Byte]]):Unit={
-
-        }
-
-        override def cancel():Unit={
-
-        }
-        }
